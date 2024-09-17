07a793da2827cac594d0ebb0691ea8c09c50ac47
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-17 17:02:33 +0500

    not = to

--- main.py
+++ main.py
@@ -15,3 +15,20 @@ if b > 0 or c > 0:
     print("either of the number is greater than 0")
 else:
     print("no number is greater than 0")
+a = 10 
+b = 12
+c = 2
+print( a != b)
+print( b != c)
+a = "python"
+b = "coding"
+if a != b :
+    print("a and b are different")
+a = 4
+b = 5
+if ( a == 1) != (b == 5):
+    print('hello')
+
+a = int(input("enter a number"))
+if a%2 != 0:
+    print("a, is not even a number.")
