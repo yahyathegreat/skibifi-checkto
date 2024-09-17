ccc2f8adc7bc6f78e641fa6dd6ba08ea61e045a9
yahyathegreat <yahyamardan1234@gmail.com>
2024-09-17 17:17:31 +0500

    bmi check you are fat

--- main.py
+++ main.py
@@ -32,3 +32,19 @@ if ( a == 1) != (b == 5):
 a = int(input("enter a number"))
 if a%2 != 0:
     print("a, is not even a number.")
+height = float(input("enter your height in cm")
+weight = float(input("enter your weight in kg")
+BMI = weight / (height/100)**2
+print("your BMI is",BMI)
+if BMI <= 18.4:
+    print("you are underweight")
+elif BMI <= 24.9:
+    print("you are healthy.")
+elif BMI <= 29.9:
+    print("you are overweight.")
+elif BMI <= 34.9:
+    print("you are severly overweight.")
+elif BMI <= 39.9:
+    print("you are obese.")
+else BMI <= 44.9:
+    print("you are severly obese.")
